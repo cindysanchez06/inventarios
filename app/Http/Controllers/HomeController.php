b9fe3974a8207ab7a8c4beaf180037e8dc7eed9f
Cindy Sanchez <csanchez@optimeconsulting.com>
2020-06-13 22:44:47 -0500

    agregando home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,6 +15,16 @@ class HomeController extends Controller
   
     public function index()
     {
-        return view('home');
+        $total = \DB::table('inventarios')->select('inventarios.*')->get();
+        $inventario = \DB::table('inventarios')->select('inventarios.*','marcas.nombre AS marca','marca_cpu.nombre as marca_cpu','marca_monitor.nombre as marca_monitor','marca_mouse.nombre as marca_mouse','procesadores.nombre AS procesador','sistemas_operativos.nombre AS sistemao','rams.nombre AS ram')
+                ->join('marcas','inventarios.marca_teclado','=','marcas.id')
+                ->join('marcas as marca_cpu','inventarios.marca_cpu','=','marca_cpu.id')
+                ->join('marcas as marca_monitor','inventarios.marca_monitor','=','marca_monitor.id')
+                ->join('marcas as marca_mouse','inventarios.marca_mouse','=','marca_mouse.id')
+                ->join('procesadores','inventarios.procesador_id','=','procesadores.id')
+                ->join('sistemas_operativos','inventarios.sistema_operativo_id','=','sistemas_operativos.id')
+                ->join('rams','inventarios.ram_id','=','rams.id')
+                ->paginate(50);
+        return view('home',compact('total','inventario'));
     }
 }
---
+++ resources/views/layouts/menuPrincipal.blade.php
@@ -0,0 +1,42 @@
+     <!-- Fixed navbar -->
+     <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="{{ url('home') }}">SPI</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="">
+              <a href="{{ route('home') }}" role="button" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-briefcase"></i> Inventarios </a>
+            </li>
+            @if(isset(Auth::user()->rol_id) && Auth::user()->rol_id == 1)
+              <li class="dropdown">
+              </li>
+            @endif
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ url('/') }}">Login</a></li>
+                            <li><a href="{{ url('/register') }}">Registrar</a></li>
+                        @else
+                            <li><a>{{ Auth::user()->cargo }}</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu" style="color: black">
+                                    <li><a href="{{ route('logout') }}"><i class="fa fa-btn fa-sign-out" style="color: black"></i>Salir</a></li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
